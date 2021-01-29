155ab1c03fed136bfee1e6c7b4652e987b903aac
brumach <brumach527@gmail.com>
2021-01-29 13:24:25 -0300

    espaço pula

--- Assets/bola.cs
+++ Assets/bola.cs
@@ -7,20 +7,43 @@ public class bola : MonoBehaviour
     private Rigidbody2D rbody;
     private float dir;
     public float speed;
-
-    // Start is called before the first frame update
+    public bool jump = false;
+    public bool grounded = false;
+    public float jumpForce;
     void Start()
     {
         rbody = gameObject.GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        groundcheck();
         dir = Input.GetAxisRaw("Horizontal");
+        if(Input.GetKeyDown("space")&& grounded)
+        {
+            jump = true;
+        }
     }
     private void FixedUpdate()
     {
         rbody.AddForce(new Vector2(dir * speed, 0));
+        if(jump)
+        {
+            rbody.AddForce(new Vector2(0, jumpForce));
+            jump = false;
+        }
+    }
+    private void groundcheck()
+    {
+        RaycastHit hit;
+        Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity);
+        Debug.DrawRay(transform.position, (Vector3.down)*6, Color.red);
+        if (hit.distance < 6)
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
     }
 }
