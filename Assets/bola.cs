aeb87feb2f84b7f8878935298d61622c5cb9474b
brumach <brumach527@gmail.com>
2021-01-29 16:29:42 -0300

    pulo que funciona

--- Assets/bola.cs
+++ Assets/bola.cs
@@ -7,6 +7,9 @@ public class bola : MonoBehaviour
     private Rigidbody2D rbody;
     private float dir;
     public float speed;
+    private bool jump = false;
+    public bool grounded = false;
+    public float jumpForce = 300;
     void Start()
     {
         rbody = gameObject.GetComponent<Rigidbody2D>();
@@ -14,9 +17,34 @@ public class bola : MonoBehaviour
     void Update()
     {
         dir = Input.GetAxisRaw("Horizontal");
+        if(Input.GetKeyDown("space"))
+        {
+            jump = true;
+        }
+
     }
     private void FixedUpdate()
     {
         rbody.AddForce(new Vector2(dir * speed, 0));
+        if(jump && grounded)
+        {
+            rbody.AddForce(new Vector2(0, jumpForce));
+            jump = false;
+        }
+    }
+    private void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.transform.tag == "ground")
+        {
+            grounded = true;
+        }
+
+    }
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.transform.tag == "ground")
+        {
+            grounded = false;
+        }
     }
 }
