fad52d16a63526e87b0de6e19125fdfbade8bcf3
brumach <brumach527@gmail.com>
2021-01-29 22:48:24 -0300

    com inimigo que anda e vira quando bate na parede

---
+++ Assets/Scripts/inimigoAnda.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inimigoAnda : MonoBehaviour
+{
+    public float dir;
+    public float speed;
+    private Rigidbody2D rbody;
+    void Start()
+    {
+        rbody = gameObject.GetComponent<Rigidbody2D>();
+        dir = Random.Range(-1, 1);
+    }
+    void Update()
+    {
+        if (dir == 0) dir = 1;
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        rbody.velocity = new Vector2(dir * speed, rbody.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.transform.tag == "wall")
+        {
+            dir = -dir;
+        }
+    }
+}
