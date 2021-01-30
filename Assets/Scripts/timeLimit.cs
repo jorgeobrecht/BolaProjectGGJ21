e348b83c74890c686cb1150e6ac6289bee670f06
jorgeobrecht <jorgeobrecht@gmail.com>
2021-01-30 03:14:07 -0300

    add partial timeLimit

---
+++ Assets/Scripts/timeLimit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class timeLimit : MonoBehaviour
+{
+    public float limit = 5.0f;
+
+    private void Update()
+    {
+        if (Time.time == limit)
+        {
+            GameOver();
+        }
+
+        /*limit -= Time.deltaTime;
+        if (limit <= 0)
+            GameOver();*/
+    }
+
+    private void GameOver()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
+
+
