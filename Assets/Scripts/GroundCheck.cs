db55054f24b41abcd40b1f5d10e90f6b18aadbae
brumach <brumach527@gmail.com>
2021-01-30 13:17:04 -0300

    player lindo e cheiroso com tds as funçoes

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour
+{
+    private PlayerController player;
+    void Start()
+    {
+        player = gameObject.GetComponentInParent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        //groundcheck
+        if (col.transform.tag == "ground")
+        {
+            player.grounded = true;
+            player.hovering = false;
+            player.hover = true;
+            player.jump = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        //groundcheck
+        if (col.transform.tag == "ground")
+        {
+            player.grounded = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,63 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //objetos
     private Rigidbody2D rbody;
     private SpriteRenderer srender;
+    private Animator anim;
+    //movimento
     private float dir;
     public float speed;
-    private bool jump = false;
+    //pulo
+    private bool jumpDown = false;
+    public bool jump = false;
     public bool grounded = false;
-    public float jumpForce ;
+    public bool hovering = false;
+    public bool hover = false;
+    public float jumpForce;
     public float fallWeight = 10;
+    public float gScale;
+    public float hoverGrav;
+    //combate
+    private bool attack = false;
+    private bool canMove = true;
+    private bool Block = false;
+    //info jogo
     public bool isAlive;
 
 
 
     void Start()
     {
+        
         rbody = gameObject.GetComponent<Rigidbody2D>();
         srender = gameObject.GetComponent<SpriteRenderer>();
+        anim = gameObject.GetComponent<Animator>();
+        rbody.gravityScale = gScale;
         isAlive = true;
     }
-
-    void getInput()
-    {
-        dir = (Input.GetAxisRaw("Horizontal"));
-        if (Input.GetKeyDown("space"))
-        {
-            jump = true;
-        }
-            
-    }
-
     void Update()
     {
-        if(isAlive)
+        if (isAlive)
             getInput();
+        AttackControl();
+        BlockControl();
 
-    }
-
-    private void MovePlayer()
-    {
-        rbody.velocity = new Vector2(dir * speed, rbody.velocity.y);    
-
-        //pulo
-        if (jump && grounded)
-        {
-            rbody.AddForce(new Vector2(0, jumpForce));
-            grounded = false;
-            jump = false;
-        }
-    /*   if (!grounded)
-        {
-            if(rbody.velocity.y <= 0)
-            {
-                rbody.AddForce(new Vector2(0,-fallWeight));
-            }
-        }*/
     }
 
     private void FixedUpdate()
@@ -65,38 +54,100 @@ public class PlayerController : MonoBehaviour
         MovePlayer();
     }
 
+    //receber informação do teclado
+    void getInput()
+    {
+        if (canMove)
+        {
+            //info movimento
+            dir = (Input.GetAxisRaw("Horizontal"));
+            if (Input.GetKeyDown("space"))
+            {
+                jump = true;
+            }
+            if (Input.GetKey("space"))
+            {
+                jumpDown = true;
+            }
+            else
+            {
+                jumpDown = false;
+            }
+            //info ataque
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                attack = true;
+                Debug.Log("ataquei");
+            }
+        }
+    }
+    private void MovePlayer()
+    {
+
+        //movimento
+        rbody.velocity = new Vector2(dir * speed, rbody.velocity.y);
+        //FLIP
+        if(dir != transform.localScale.x && dir != 0)
+        {
+            transform.localScale = new Vector2(dir, 1);
+        }
+        //pulo
+        if (jump)
+        {
+            if (grounded)
+            {
+                rbody.velocity = (new Vector2(rbody.velocity.x, jumpForce));
+                grounded = false;
+                jump = false;
+            }
+            else if(!grounded && hover)
+            {
+                rbody.velocity = (new Vector2(rbody.velocity.x, jumpForce));
+                hover = false;
+                hovering = true;
+            }
+        }
+        if(hovering && jumpDown && rbody.velocity.y < 0)
+        {
+            rbody.gravityScale = hoverGrav;
+        }
+        else
+        {
+            rbody.gravityScale = gScale; 
+        }
+    }
+
+    private void AttackControl()
+    {
+        if (attack)
+        {
+
+        }
+    }
+    private void BlockControl()
+    {
+
+    }
     private void OnCollisionStay2D(Collision2D col)
     {
-        if(col.transform.tag == "ground")
-        {
-            grounded = true;
-        }
+        
+        //check colisão com inimigo
         if(col.transform.tag == "enemy" && isAlive)
         {
+            Debug.Log("perdeu");
             srender.color = Color.black;
             isAlive = false;
-            GameController.PlayerLost();
         }
 
 
     }
-
-    private void OnCollisionExit2D(Collision2D col)
-    {
-        if (col.transform.tag == "ground")
-        {
-            grounded = false;
-        }
-        
-    }
-
     // GANHOU
     private void OnTriggerEnter2D(Collider2D collision)
     { 
+        //check de vitoria
         if (collision.transform.tag == "victory")
         {
-            GameController.PlayerWon();
-                       
+            GameController.PlayerWon();  
         }
     }
 
