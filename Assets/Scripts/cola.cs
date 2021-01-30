544c877605cf7656ed68945d184359dde9309853
jorgeobrecht <jorgeobrecht@gmail.com>
2021-01-30 18:04:34 -0300

    Colinha enemy

---
+++ Assets/Scripts/cola.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cola : MonoBehaviour
+{
+    public float speed = 50.0f;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed*transform.localScale.x, 0);
+    }
+
+  
+    void Update()
+    {
+       
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+            Destroy(this.gameObject);
+
+    }
+
+}
