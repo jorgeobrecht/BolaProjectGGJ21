3f396386808f9b58e77c7edaa057bb390a5fa9ae
brumach <brumach527@gmail.com>
2021-01-30 20:23:49 -0300

    grampeador finalizado

--- Assets/Scripts/inimigoAnda.cs
+++ Assets/Scripts/inimigoAnda.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class inimigoAnda : MonoBehaviour
 {
-    public float dir;
     public float speed;
+    public float deathForce;
+    public bool vivo = true;
     private Rigidbody2D rbody;
+    private BoxCollider2D colid;
     void Start()
     {
         rbody = gameObject.GetComponent<Rigidbody2D>();
+        colid = gameObject.GetComponent<BoxCollider2D>();
 
     }
     void Update()
@@ -22,20 +25,46 @@ public class inimigoAnda : MonoBehaviour
     }
     private void Move()
     {
-        rbody.velocity = new Vector2(dir * speed, rbody.velocity.y);
+        rbody.velocity = new Vector2(transform.localScale.x * speed, rbody.velocity.y);
+    }
+    public void Flip()
+    {
+        if (vivo)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, 1, 1);
+        }
     }
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.transform.tag == "wall" || col.transform.tag == "enemy")
         {
-            dir = -dir;
+            Flip();
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "victory")
         {
-            dir = -dir;
+            Flip();
+        }
+        if (collision.tag == "ataque")
+        {
+            Morto();
+        }
+    }
+    private void Morto()
+    {
+        vivo = false;
+        speed = 0;
+        transform.localScale = new Vector3(1, -1, 1);
+        rbody.AddForce(Vector2.up * deathForce);
+        colid.enabled = false;
+    }
+    private void OnBecameInvisible()
+    {
+        if(!vivo)
+        {
+            Destroy(gameObject);
         }
     }
 }
