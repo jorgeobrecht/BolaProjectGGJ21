70c0b92c9df5f17e82ddcbfc7ce64b9f6cb423e9
leogiuris <leogiuris@gmail.com>
2021-01-30 17:13:38 -0300

    menus funcionando

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class StartMenu : MonoBehaviour
+{
+    
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("SampleScene2");
+    }
+
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/credits.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class credits : MonoBehaviour
+{
+    public GameObject creditos;
+    string textoCreditos;
+    Vector3 rollSpeed = new Vector3(0, 1f, 0);
+    // Start is called before the first frame update
+    void Start()
+    {
+        textoCreditos = "comi teu cu";
+        creditos.GetComponent<Text>().text = textoCreditos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        creditos.transform.position += rollSpeed;
+
+        if (creditos.transform.position.y >= 350)
+            SceneManager.LoadScene("StartMenu");
+    }
+}
