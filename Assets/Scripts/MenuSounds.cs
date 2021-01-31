19fe9d0777f5e483c4ce85750bf296beeccd697d
leogiuris <leogiuris@gmail.com>
2021-01-31 15:48:50 -0300

    creditos

--- Assets/Scripts/MenuSounds.cs
+++ Assets/Scripts/MenuSounds.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class MenuSounds : MonoBehaviour
 {
-    int n = 0;
+  
     
 
     public void EnterSound()
     {
-        n = (int)Random.Range(0, 3);
-        if (n != 0)
-            SoundManagerScript.PlaySound("MenuCursor" + n);
-        else
-            SoundManagerScript.PlaySound("MenuCursor");
-        //SoundManagerScript.PlaySound("Attack");
-        Debug.Log(n);
+        //n = (int)Random.Range(0, 3);
+        //Debug.Log(n);
+        //if (n != 0)
+        //    SoundManagerScript.PlaySound("MenuCursor" + n);
+        //else
+        //    SoundManagerScript.PlaySound("MenuCursor");
+        ////SoundManagerScript.PlaySound("Attack");
+        //Debug.Log(n);
         
     }
 }
