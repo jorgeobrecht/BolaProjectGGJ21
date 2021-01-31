16931c7ee00bcfb4fa189fec4ae16ccc6786d92c
leogiuris <leogiuris@gmail.com>
2021-01-31 12:56:22 -0300

    camera do mario, e menus funcionam

--- Assets/Scripts/follow.cs
+++ Assets/Scripts/follow.cs
@@ -7,19 +7,24 @@ public class follow : MonoBehaviour
     
     public Transform bola;
     public float altura;
+    public float xOffset;
+    private float xBarreira;
     public Vector3 v;
+    public Transform barreira;
 
     // Start is called before the first frame update
     void Start()
     {
+        xBarreira = barreira.position.x;
         altura = bola.position.y;
-        v = new Vector3(bola.position.x, altura, transform.position.z);
+        v = new Vector3(xBarreira + xOffset, altura, transform.position.z);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        v.x = bola.position.x;
+        if(bola.position.x > xBarreira + xOffset)
+            v.x = bola.position.x;
         if(bola.position.y > 12)
             v.y = altura + (bola.position.y - 12);
 
